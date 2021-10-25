9fd451c44eb284e41234c4793ccdf6a94647ea0b
Miftahul Arifin <miftahul97@gmail.com>
2021-10-25 08:45:39 +0700

    update project

--- src/repositories/projects.js
+++ src/repositories/projects.js
@@ -1,6 +1,7 @@
 const data = [
   {
     id: 1,
+    year: '2021',
     title: 'To Do List App',
     description: `This project was made in as a way to participate Gethired Devcode Front-end Challenge. I
       also used it to learn how to implement an UI design to Front-end Code and integration from Back-end using API. 
@@ -22,25 +23,54 @@ const data = [
   },
   {
     id: 2,
-    title: 'Monitoring System',
-    description: `This project was made in as a way to practice my GraphQL skills. I
-      also used it to learn how to implement an ORM into my back-end, which
-      in this case I used Sequelize. It replicats the famous column layout
-      that Pinteres uses, and it exemplifies several small features present
-      in the real website. I made a GraphQL API for my backend using Apollo
-      Server, which allowed a wall between the clients and the API. The user
-      can also login with their google account, and all info related to the
-      user is saved in a ClearDB MySQL database. If the user wants to upload
-      a pin, the image (and other details) are inserted to the database and
-      uploaded to Cloudinary as an image storage.`,
+    year: '2021',
+    title: 'Smartmeter: IoT Monitoring',
+    description: `This project was made during working in PT Elnusa Tbk. Smartmeter is an IoT monitoring application system which
+      is connected with the devices such as household water measurement and gas measurement. I used VueJS as Front-end and MoleculerJS as
+      back-end microservices. The user could figure out the information from the dashboard and set up the device directly from the app.
+    `,
     skill:
       'NodeJS, Express, PostgreSQL, VueJS, MoleculerJS, Microservices, MQTT, AMQP',
     source: '',
-    preview: '',
+    preview: 'https://iot.elnusa.co.id/smartmeter',
     images: [
       {
-        url: 'https://cdn.mos.cms.futurecdn.net/jU8HAQWqxPMgH3g6qSbA5.jpg',
-        description: 'Images'
+        url: require('@/assets/images/projects/smartmeter-1.jpeg'),
+        description: 'Smartmeter'
+      },
+      {
+        url: require('@/assets/images/projects/smartmeter-2.jpeg'),
+        description: 'Smartmeter'
+      },
+      {
+        url: require('@/assets/images/projects/smartmeter-3.jpeg'),
+        description: 'Smartmeter'
+      }
+    ]
+  },
+  {
+    id: 3,
+    year: '2020',
+    title: 'DiRumahAja: Online Presence',
+    description: `This project was made during working in PT Elnusa Tbk. DiRumahAja is an online presence app for employee. It records thousands user
+      activities such as presence and geolocation. It built using VueJS as Front-end and NodeJS microservice architecture with MQTT broker as back-end.
+      The user could submit online presence through the app and then the admin user could figure out the dashboard.
+    `,
+    skill: 'NodeJS, Express, PostgreSQL, VueJS, Microservices, MQTT, AMQP',
+    source: '',
+    preview: 'https://dirumahaja.elnusa.co.id/',
+    images: [
+      {
+        url: require('@/assets/images/projects/dirumahaja-1.jpeg'),
+        description: 'DiRumahAja'
+      },
+      {
+        url: require('@/assets/images/projects/dirumahaja-2.jpeg'),
+        description: 'DiRumahAja'
+      },
+      {
+        url: require('@/assets/images/projects/dirumahaja-3.jpeg'),
+        description: 'DiRumahAja'
       }
     ]
   }
